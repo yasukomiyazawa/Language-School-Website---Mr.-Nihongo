0e18eba812927309cd2158bfdd6f0df176016034
Yasuko Miyazawa <yasukomiyazawa08@gmail.com>
2023-10-12 16:40:32 +0900

    modified the schedule section. added a google calendar embed and also added the new letter subscription section. lastly, also added the created by section in the bottom of the footer

--- app/page.tsx
+++ app/page.tsx
@@ -48,30 +48,30 @@ export default function Home() {
                 style={{ color: "red" }}
               />
             </div>
-            <div>mreigo@gmail.com</div>
+            <div className="hover:text-red-700 cursor-pointer">
+              mreigo@gmail.com
+            </div>
           </div>
         </div>
 
         <div className="flex items-center">
-          <div>
+          <div className="hover:text-red-500">
             <FontAwesomeIcon
               icon={faFacebook}
-              className="w-3 mr-4"
-              style={{ color: "lightslategray" }}
+              className="w-3 mr-4 text-lightslategray group-hover:text-red-500 cursor-pointer"
             />
           </div>
-          <div>
+          <div className="group">
             <FontAwesomeIcon
               icon={faInstagram}
-              className="w-3 mr-4"
-              style={{ color: "lightslategray" }}
+              className="w-3 mr-4 text-lightslategray group-hover:text-red-500 cursor-pointer"
             />
           </div>
+
           <div>
             <FontAwesomeIcon
               icon={faTwitter}
-              className="w-3 mr-4"
-              style={{ color: "lightslategray" }}
+              className="w-3 mr-4 text-lightslategray group-hover:text-red-500 cursor-pointer"
             />
           </div>
         </div>
@@ -119,7 +119,7 @@ export default function Home() {
         </Link>
       </div>
 
-      <div className="bg-sched-bg bg-cover h-screen">
+      {/* <div className="bg-sched-bg bg-cover h-screen">
         <h2 className="flex justify-center mt-28 text-5xl font-bold text-red-800 underline underline-offset-8">
           2023 Schedule
         </h2>
@@ -254,10 +254,133 @@ export default function Home() {
             </div>
           </div>
         </div>
+      </div> */}
+
+      <div className="grid grid-cols-4 w-full h-screen gap-x-8 my-3 px-8 box-border">
+        <div className="col-span-2 pt-36">
+          <h3 className="pb-8 text-3xl font-medium text-red-800">
+            Event Calendars
+          </h3>
+          <iframe
+            src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Asia%2FTokyo&showNav=1&showDate=1&showPrint=1&showTabs=1&showTz=0&showCalendars=1&title=2023%20Schedule&src=eWFzdWtvbWl5YXphd2EwOEBnbWFpbC5jb20&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4tZ2IuamFwYW5lc2UjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23039BE5&color=%2333B679&color=%230B8043"
+            style={{ border: 0 }}
+            width=""
+            height=""
+            scrolling="no"
+            className="w-full h-3/4"
+          ></iframe>
+        </div>
+        <div className="flex flex-col col-span-1 pt-36">
+          <h3 className="pb-20 text-3xl font-medium text-red-800">
+            Latest Events
+          </h3>
+          <div className="flex gap-3 mb-11">
+            <div className="flex flex-col items-center">
+              <span className="text-sm">OCT</span>
+              <span className="text-xl font-semibold text-gray-600">10</span>
+            </div>
+            <span className="text-base font-medium">
+              1st Day of Autumn Semester
+            </span>
+          </div>
+          <div className="flex gap-3 mb-11">
+            <div className="flex flex-col items-center">
+              <span className="text-sm">OCT</span>
+              <span className="text-xl font-semibold text-gray-600">27</span>
+            </div>
+            <span className="text-base font-medium">
+              October Orientation <br />
+              (New Students of October Intake)
+            </span>
+          </div>
+          <div className="flex gap-3">
+            <div className="flex flex-col items-center">
+              <span className="text-sm">OCT</span>
+              <span className="text-xl font-semibold text-gray-600">31</span>
+            </div>
+            <span className="text-base font-medium">Halloween Party</span>
+          </div>
+        </div>
+        <div className="flex flex-col col-span-1 pt-36">
+          <h3 className="pb-20 text-3xl font-medium text-red-800">
+            Upcoming Courses
+          </h3>
+          <div className="grid grid-rows-4">
+            <Link
+              href="/Courses"
+              className="row-span-1 border border-gray-500 border-l-4 border-l-red-500 hover:bg-red-700"
+            >
+              <div className="flex flex-col items-center py-3 hover:text-white">
+                <span className="text-lg font-medium hover:font-semibold mb-2">
+                  Language and Culture
+                </span>
+                <span>Thursday, 10am - 12pm</span>
+              </div>
+            </Link>
+
+            <Link
+              href="/Courses"
+              className="row-span-1 border border-gray-500 border-l-4 border-l-red-500 hover:bg-red-700"
+            >
+              <div className="flex flex-col items-center py-3 hover:text-white">
+                <span className="text-lg font-medium hover:font-semibold mb-2">
+                  Pronunciation Improvement
+                </span>
+                <span>Friday, 10am - 12pm</span>
+              </div>
+            </Link>
+
+            <Link
+              href="/Courses"
+              className="row-span-1 border border-gray-500 border-l-4 border-l-red-500 hover:bg-red-700"
+            >
+              <div className="flex flex-col items-center py-3 hover:text-white">
+                <span className="text-lg font-medium hover:font-semibold mb-2">
+                  Exam Preparation
+                </span>
+                <span>Monday, 10am - 12pm</span>
+              </div>
+            </Link>
+
+            <Link
+              href="/Courses"
+              className="row-span-1 border border-gray-500 border-l-4 border-l-red-500 hover:bg-red-700"
+            >
+              <div className="flex flex-col items-center py-3 hover:text-white">
+                <span className="text-lg font-medium hover:font-semibold mb-2">
+                  Children's English
+                </span>
+                <span>Tuesday, 10am - 12pm</span>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center bg-red-800 py-16 mb-10 gap-5">
+        <div className="text-3xl text-white font-bold">Subscribe Now!</div>
+        <div className="text-sm text-gray-200">
+          Subscribe for Exclusive Course Updates, Language Lessons, and Exciting
+          Language Learning Adventures!
+        </div>
+        <div className="xl:w-[800px] border border-gray-300 rounded-full py-2 px-3 flex">
+          <input
+            id="email"
+            type="email"
+            placeholder="enter your email"
+            className="outline-none bg-transparent mr-2 w-full"
+          />
+          <button
+            className="border border-gray-200 bg-white rounded-full text-black py-1 px-4 hover:bg-gray-500 hover:text-white hover:text-medium"
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
       </div>
 
       <div>
-        <h2 className="flex justify-center m-8 text-5xl font-bold text-red-800 underline underline-offset-8">
+        <h2 className="flex justify-center mb-11 text-4xl font-bold text-red-800 underline underline-offset-8">
           Featured Courses
         </h2>
         <div className="grid grid-cols-4 gap-5 m-8">
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,101 +11,114 @@ import {
 
 const Footer = () => {
   return (
-    <div className="grid grid-cols-4 gap-10 items-center bg-footer-bg bg-cover bg-right p-14">
-      <div className="col-span-1">
-        <Link href="/">
-          <Image
-            src="/logo.png"
-            alt="logo"
-            width={150}
-            height={10}
-            className="w-36 h-auto"
-          />
-        </Link>
-        <p className="text-gray-500 font-semibold">
-          Elevate Your English: Empowering Futures, One Word at a Time!
-        </p>
-      </div>
-      <div className="col-span-1">
-        <div className="flex flex-col items-center justify-center">
-          <h2 className="text-center mb-8 font-bold text-red-700 text-lg border-b-2 border-neutral-500">
-            About School
-          </h2>
-          <ul className="flex flex-col items-center gap-2 text-gray-500 cursor-pointer">
-            <li className="hover:font-semibold hover:text-red-600">About Us</li>
-            <li className="hover:font-semibold hover:text-red-600">
-              Our Staff
-            </li>
-            <li className="hover:font-semibold hover:text-red-600">
-              Our Partners
-            </li>
-            <li className="hover:font-semibold hover:text-red-600">Careers</li>
-          </ul>
+    <>
+      <div className="grid grid-cols-4 gap-10 items-center bg-footer-bg bg-cover bg-right p-14">
+        <div className="col-span-1">
+          <Link href="/">
+            <Image
+              src="/logo.png"
+              alt="logo"
+              width={150}
+              height={10}
+              className="w-36 h-auto"
+            />
+          </Link>
+          <p className="text-gray-500 font-semibold">
+            Elevate Your English: Empowering Futures, One Word at a Time!
+          </p>
+        </div>
+        <div className="col-span-1">
+          <div className="flex flex-col items-center justify-center">
+            <h2 className="text-center mb-8 font-bold text-red-700 text-lg border-b-2 border-neutral-500">
+              About School
+            </h2>
+            <ul className="flex flex-col items-center gap-2 text-gray-500 cursor-pointer">
+              <li className="hover:font-semibold hover:text-red-600">
+                About Us
+              </li>
+              <li className="hover:font-semibold hover:text-red-600">
+                Our Staff
+              </li>
+              <li className="hover:font-semibold hover:text-red-600">
+                Our Partners
+              </li>
+              <li className="hover:font-semibold hover:text-red-600">
+                Careers
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-span-1">
+          <div className="flex flex-col items-center justify-center cursor-pointer">
+            <h2 className="text-center mb-8 font-bold text-red-700 text-lg border-b-2 border-neutral-500">
+              Top Courses
+            </h2>
+            <ul className="flex flex-col items-center gap-2 text-gray-500">
+              <li className="hover:font-semibold hover:text-red-600">
+                General English
+              </li>
+              <li className="hover:font-semibold hover:text-red-600">
+                Business English
+              </li>
+              <li className="hover:font-semibold hover:text-red-600">
+                Exam Preparation
+              </li>
+              <li className="hover:font-semibold hover:text-red-600">
+                Conversation and Fluency
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-span-1">
+          <div className="flex flex-col items-center justify-center cursor-pointer">
+            <h2 className="text-center mb-8 font-bold text-red-700 text-lg border-b-2 border-neutral-500">
+              Follow Us
+            </h2>
+            <ul className="flex flex-col items-center gap-2 text-gray-500">
+              <li className="flex hover:font-semibold hover:text-red-600">
+                <FontAwesomeIcon
+                  icon={faFacebook}
+                  className="w-4 mr-2"
+                  style={{ color: "red" }}
+                />
+                Facebook
+              </li>
+              <li className="flex hover:font-semibold hover:text-red-600">
+                <FontAwesomeIcon
+                  icon={faInstagram}
+                  className="w-4 mr-2"
+                  style={{ color: "red" }}
+                />
+                Instagram
+              </li>
+              <li className="flex hover:font-semibold hover:text-red-600">
+                <FontAwesomeIcon
+                  icon={faTwitter}
+                  className="w-4 mr-2"
+                  style={{ color: "red" }}
+                />
+                Twitter
+              </li>
+              <li className="flex hover:font-semibold hover:text-red-600">
+                <FontAwesomeIcon
+                  icon={faDiscord}
+                  className="w-4 mr-2"
+                  style={{ color: "red" }}
+                />
+                Discord
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-      <div className="col-span-1">
-        <div className="flex flex-col items-center justify-center cursor-pointer">
-          <h2 className="text-center mb-8 font-bold text-red-700 text-lg border-b-2 border-neutral-500">
-            Top Courses
-          </h2>
-          <ul className="flex flex-col items-center gap-2 text-gray-500">
-            <li className="hover:font-semibold hover:text-red-600">
-              General English
-            </li>
-            <li className="hover:font-semibold hover:text-red-600">
-              Business English
-            </li>
-            <li className="hover:font-semibold hover:text-red-600">
-              Exam Preparation
-            </li>
-            <li className="hover:font-semibold hover:text-red-600">
-              Conversation and Fluency
-            </li>
-          </ul>
-        </div>
+      <div className="flex justify-center mt-5">
+        Developed by
+        <span className="text-red-800 px-1 font-semibold">
+          Yasuko Miyazawa
+        </span>{" "}
+        | All Rights Reserved.
       </div>
-      <div className="col-span-1">
-        <div className="flex flex-col items-center justify-center cursor-pointer">
-          <h2 className="text-center mb-8 font-bold text-red-700 text-lg border-b-2 border-neutral-500">
-            Follow Us
-          </h2>
-          <ul className="flex flex-col items-center gap-2 text-gray-500">
-            <li className="flex hover:font-semibold hover:text-red-600">
-              <FontAwesomeIcon
-                icon={faFacebook}
-                className="w-4 mr-2"
-                style={{ color: "red" }}
-              />
-              Facebook
-            </li>
-            <li className="flex hover:font-semibold hover:text-red-600">
-              <FontAwesomeIcon
-                icon={faInstagram}
-                className="w-4 mr-2"
-                style={{ color: "red" }}
-              />
-              Instagram
-            </li>
-            <li className="flex hover:font-semibold hover:text-red-600">
-              <FontAwesomeIcon
-                icon={faTwitter}
-                className="w-4 mr-2"
-                style={{ color: "red" }}
-              />
-              Twitter
-            </li>
-            <li className="flex hover:font-semibold hover:text-red-600">
-              <FontAwesomeIcon
-                icon={faDiscord}
-                className="w-4 mr-2"
-                style={{ color: "red" }}
-              />
-              Discord
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
